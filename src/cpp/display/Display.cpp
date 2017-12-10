e1a9c4dad9b53421b33607cb1fdc5e825db100e6
Jan Van den Bergh <jan.vdbergh@gmail.com>
2017-12-10 13:50:46 +0100

    Do not crash on missing image or animation.

--- src/cpp/display/Display.cpp
+++ src/cpp/display/Display.cpp
@@ -1,20 +1,38 @@
 #include "Display.h"
 
 void Display::DefineImage(const std::string &name, const ImagePtr &image) {
-    _images[name] = image;
+	_images[name] = image;
+}
+
+const ImagePtr Display::GetImage(const std::string &name) const {
+	auto iterator = _images.find(name);
+	if (iterator != _images.end()) {
+		return iterator->second;
+	}
+
+	return nullptr;
 }
 
 void Display::DefineAnimation(const std::string &name, const CommandPtr &command) {
-    _animations[name] = command;
+	_animations[name] = command;
+}
+
+const CommandPtr Display::GetAnimation(const std::string &name) const {
+	auto iterator = _animations.find(name);
+	if (iterator != _animations.end()) {
+		return iterator->second;
+	}
+
+	return nullptr;
 }
 
 void Display::Sleep(uint16_t millis) {
-    usleep((uint32_t)1000 * millis);
+	usleep((uint32_t) 1000 * millis);
 }
 
-void Display::ShowAnimation(const std::string & name) {
-    CommandPtr command = GetAnimation(name);
-    if (command) {
-        this->ExecuteCommand(command);
-    }
+void Display::ShowAnimation(const std::string &name) {
+	CommandPtr command = GetAnimation(name);
+	if (command) {
+		this->ExecuteCommand(command);
+	}
 }
--- src/cpp/display/Display.h
+++ src/cpp/display/Display.h
@@ -8,52 +8,48 @@
 
 class Display {
 public:
-    virtual ~Display() = default;
+	virtual ~Display() = default;
 
-    void ExecuteCommand(const CommandPtr &command) {
-        command->Visit(*this);
-    }
+	void ExecuteCommand(const CommandPtr &command) {
+		command->Visit(*this);
+	}
 
-    virtual bool Initialize(int argc, char **argv) =0;
+	virtual bool Initialize(int argc, char **argv) =0;
 
-    virtual void SetColor(uint8_t r, uint8_t g, uint8_t b) =0;
+	virtual void SetColor(uint8_t r, uint8_t g, uint8_t b) =0;
 
-    virtual void Clear() =0;
+	virtual void Clear() =0;
 
-    virtual void DrawPixel(int16_t x, int16_t y) =0;
+	virtual void DrawPixel(int16_t x, int16_t y) =0;
 
-    virtual void DrawRectangle(int16_t x, int16_t y, uint16_t width, uint16_t height) =0;
+	virtual void DrawRectangle(int16_t x, int16_t y, uint16_t width, uint16_t height) =0;
 
-    virtual void DrawDigit(uint8_t position, uint8_t digit) =0;
+	virtual void DrawDigit(uint8_t position, uint8_t digit) =0;
 
-    virtual void DrawSmallText(int16_t x, int16_t y, std::string text) =0;
+	virtual void DrawSmallText(int16_t x, int16_t y, std::string text) =0;
 
-    virtual void DrawLargeText(int16_t x, int16_t y, std::string text) =0;
+	virtual void DrawLargeText(int16_t x, int16_t y, std::string text) =0;
 
-    virtual void DrawImage(int16_t x, int16_t y, const std::string &imageName) =0;
+	virtual void DrawImage(int16_t x, int16_t y, const std::string &imageName) =0;
 
-    virtual void ShowAnimation(const std::string &name);
+	virtual void ShowAnimation(const std::string &name);
 
-    virtual void Sleep(uint16_t millis);
+	virtual void Sleep(uint16_t millis);
 
-    virtual void Show() =0;
+	virtual void Show() =0;
 
-    void DefineImage(const std::string &name, const ImagePtr &image);
+	void DefineImage(const std::string &name, const ImagePtr &image);
 
-    virtual void DefineAnimation(const std::string &name, const CommandPtr &command);
+	virtual void DefineAnimation(const std::string &name, const CommandPtr &command);
 
 protected:
-    const ImagePtr GetImage(const std::string &name) const {
-        return _images.at(name);
-    }
+	const ImagePtr GetImage(const std::string &name) const;
 
-    const CommandPtr GetAnimation(const std::string &name) const {
-        return _animations.at(name);
-    }
+	const CommandPtr GetAnimation(const std::string &name) const;
 
 private:
-    std::map<std::string, ImagePtr> _images;
-    std::map<std::string, CommandPtr> _animations;
+	std::map<std::string, ImagePtr> _images;
+	std::map<std::string, CommandPtr> _animations;
 };
 
 boost::shared_ptr<Display> CreateDisplay();
