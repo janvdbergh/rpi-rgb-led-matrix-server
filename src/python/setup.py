878f9e17ce9055d4803eea09a9d1905f0f7d2942
Jan Van den Bergh <jan.vdbergh@gmail.com>
2017-12-11 17:07:25 +0100

    Setup Python module.

---
+++ src/python/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='led_matrix_client',
+	  version='1.0',
+	  description='Python led Matrix Client',
+	  author='Jan Van den Bergh',
+	  author_email='jan.vdbergh@gmail.com',
+	  packages=['led_matrix_client'])
