d72ae3950bd4ab7b11cbb85d1194d4710cd18961
Jan Van den Bergh <jan.vdbergh@gmail.com>
2018-06-06 21:42:12 +0200

    Add layer support to Python client.

--- src/python/led_matrix_client/Command.py
+++ src/python/led_matrix_client/Command.py
@@ -19,6 +19,8 @@ class CommandCode(Enum):
 	COMPOSITE = 12
 	DEFINE_IMAGE = 13
 	DEFINE_ANIMATION = 14
+	SET_LAYER = 15
+	CLEAR_LAYER = 16
 
 
 class Command:
@@ -36,7 +38,8 @@ class Command:
 
 	@staticmethod
 	def color(r, g, b):
-		return Command(Util.to16BitUnsigned(CommandCode.COLOR.value) + Util.to8BitUnsigned(r) + Util.to8BitUnsigned(g) + Util.to8BitUnsigned(b))
+		return Command(Util.to16BitUnsigned(CommandCode.COLOR.value) + Util.to8BitUnsigned(r) + Util.to8BitUnsigned(
+			g) + Util.to8BitUnsigned(b))
 
 	@staticmethod
 	def pixel(x, y):
@@ -45,7 +48,8 @@ class Command:
 	@staticmethod
 	def rectangle(x, y, width, height):
 		return Command(
-			Util.to16BitUnsigned(CommandCode.RECTANGLE.value) + Util.to16BitSigned(x) + Util.to16BitSigned(y) + Util.to16BitUnsigned(width) + Util.to16BitUnsigned(height))
+			Util.to16BitUnsigned(CommandCode.RECTANGLE.value) + Util.to16BitSigned(x) + Util.to16BitSigned(
+				y) + Util.to16BitUnsigned(width) + Util.to16BitUnsigned(height))
 
 	@staticmethod
 	def digit(position, digit):
@@ -54,16 +58,19 @@ class Command:
 
 	@staticmethod
 	def smallText(x, y, text):
-		return Command(Util.to16BitUnsigned(CommandCode.SMALL_TEXT.value) + Util.to16BitSigned(x) + Util.to16BitSigned(y) + Util.toString(text))
+		return Command(Util.to16BitUnsigned(CommandCode.SMALL_TEXT.value) + Util.to16BitSigned(x) + Util.to16BitSigned(
+			y) + Util.toString(text))
 
 	@staticmethod
 	def largeText(x, y, text):
-		return Command(Util.to16BitUnsigned(CommandCode.LARGE_TEXT.value) + Util.to16BitSigned(x) + Util.to16BitSigned(y) + Util.toString(text))
+		return Command(Util.to16BitUnsigned(CommandCode.LARGE_TEXT.value) + Util.to16BitSigned(x) + Util.to16BitSigned(
+			y) + Util.toString(text))
 
 	@staticmethod
 	def image(x, y, name):
 		return Command(
-			Util.to16BitUnsigned(CommandCode.IMAGE.value) + Util.to16BitSigned(x) + Util.to16BitSigned(y) + Util.toString(name))
+			Util.to16BitUnsigned(CommandCode.IMAGE.value) + Util.to16BitSigned(x) + Util.to16BitSigned(
+				y) + Util.toString(name))
 
 	@staticmethod
 	def animation(name):
@@ -85,16 +92,26 @@ class Command:
 	def defineImage(name, image):
 		width = image.size[0]
 		height = image.size[1]
-		data = Util.to16BitUnsigned(CommandCode.DEFINE_IMAGE.value) + Util.toString(name) + Util.to16BitUnsigned(width) + Util.to16BitUnsigned(height)
+		data = Util.to16BitUnsigned(CommandCode.DEFINE_IMAGE.value) + Util.toString(name) + Util.to16BitUnsigned(
+			width) + Util.to16BitUnsigned(height)
 		for x in range(0, width):
 			for y in range(0, height):
 				pixel = image.getpixel((x, y))
-				data = data + Util.to8BitUnsigned(pixel[0]) + Util.to8BitUnsigned(pixel[1]) + Util.to8BitUnsigned(pixel[2])
+				data = data + Util.to8BitUnsigned(pixel[0]) + Util.to8BitUnsigned(pixel[1]) + Util.to8BitUnsigned(
+					pixel[2])
 		return Command(data)
 
 	@staticmethod
 	def defineAnimation(name, command):
 		return Command(Util.to16BitUnsigned(CommandCode.DEFINE_ANIMATION.value) + Util.toString(name) + command.data)
+
+	@staticmethod
+	def setLayer(layer):
+		return Command(Util.to16BitUnsigned(CommandCode.SET_LAYER.value) + Util.to8BitUnsigned(layer))
+
+	@staticmethod
+	def clearLayer():
+		return Command(Util.to16BitUnsigned(CommandCode.CLEAR_LAYER.value))
 
 	def __init__(self, data):
 		self.data = Util.to16BitUnsigned(0xC0) + data
