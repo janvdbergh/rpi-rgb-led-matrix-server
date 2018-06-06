553e000beb3da7a88a13dde74f00e7c7fbab94af
Jan Van den Bergh <jan.vdbergh@gmail.com>
2018-06-06 21:35:56 +0200

    Use three part version number.

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='led_matrix_client',
-	  version='1.0',
+	  version='1.0.0',
 	  description='Python led Matrix Client',
 	  author='Jan Van den Bergh',
 	  author_email='jan.vdbergh@gmail.com',
